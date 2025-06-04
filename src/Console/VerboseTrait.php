b82acf96e6a4a0470836f218c1ea1082cabec8b0
Martín <820694+Martin17@users.noreply.github.com>
2025-06-04 10:38:20 -0300

    Add icon property to VerboseTrait for enhanced console output formatting

--- src/Console/VerboseTrait.php
+++ src/Console/VerboseTrait.php
@@ -11,6 +11,8 @@ namespace Tabula17\Satelles\Utilis\Console;
 trait VerboseTrait
 {
 
+    private string $icon = '🛰️';
+
     /**
      * Formats a message for console output with a timestamp and class name.
      *
@@ -20,12 +22,13 @@ trait VerboseTrait
      */
     private function formatConsoleMessage(string $message, array $context = []): string
     {
-        $formatted = date('[Y-m-d H:i:s]') . "🛰️ [" . str_replace(__NAMESPACE__ . '\\', '', __CLASS__) . "] " . $message;
+        $formatted = date('[Y-m-d H:i:s]') . $this->icon . " [" . str_replace(__NAMESPACE__ . '\\', '', __CLASS__) . "] " . $message;
         if (!empty($context)) {
             $formatted .= ' ' . json_encode($context);
         }
         return $formatted . PHP_EOL;
     }
+
     /**
      * Outputs a message to the console if verbose mode is enabled.
      *
@@ -38,5 +41,6 @@ trait VerboseTrait
             echo $this->formatConsoleMessage($message, $context);
         }
     }
+
     abstract private function isVerbose(): bool;
 }
