8049fec0e89114376efe1431815d3c98b9dab58c
Martín <820694+Martin17@users.noreply.github.com>
2025-06-04 14:07:31 -0300

    Refactor Log class to simplify method visibility and enhance VerboseTrait integration

--- src/Console/Log.php
+++ src/Console/Log.php
@@ -9,23 +9,25 @@ class Log implements LoggerInterface
 
     use VerboseTrait {
         isVerbose as private;
-        log as public ;
-        notice as public ;
-        warning as public ;
-        error as public ;
-        critical as public ;
-        alert as public ;
-        emergency as public ;
-        debug as public ;
-        info as public ;
+        log as public;
+        notice as public;
+        warning as public;
+        error as public;
+        critical as public;
+        alert as public;
+        emergency as public;
+        debug as public;
+        info as public;
 
     }
 
 
-    public function __construct(private readonly int $verboseLevel = 4, private readonly bool $useEnv = false, string $verboseIcon = '📺')
+    public function __construct(private readonly int $verboseLevel = 4, private readonly bool $useEnv = false, string $verboseContext = 'LOG', string $verboseIcon = '📺')
     {
         $this->verboseIcon = $verboseIcon;
+        $this->verboseContext = $verboseContext;
     }
+
     private function isVerbose(int $level): bool
     {
 
