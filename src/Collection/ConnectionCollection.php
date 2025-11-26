c024c7468cbe841fd8b12f54f365d78088c5d1b1
Martín Panizzo <marpan@papeleratucuman.com.ar>
2025-11-26 12:09:54 -0300

    Add static method `fromArray` to `ConnectionCollection`

--- src/Collection/ConnectionCollection.php
+++ src/Collection/ConnectionCollection.php
@@ -8,8 +8,15 @@ use Tabula17\Satelles\Utilis\Config\ConnectionConfig;
 class ConnectionCollection extends GenericCollection
 {
 
+    public static string $type = ConnectionConfig::class;
+
     public function __construct(ConnectionConfig...$connections)
     {
         $this->values = $connections;
+    }
+
+    public static function fromArray(array $config): self
+    {
+        return new self(...array_map(static fn($item) => $item instanceof self::$type ? $item : new self::$type($item), $config));
     }
 }
