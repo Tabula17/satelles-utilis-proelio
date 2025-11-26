c71cb283759290d76896e19de78e81cdff6f95d3
Martín Panizzo <marpan@papeleratucuman.com.ar>
2025-11-26 12:13:31 -0300

    Add utility methods for filtering, finding, and removing connections in `ConnectionCollection`

--- src/Collection/ConnectionCollection.php
+++ src/Collection/ConnectionCollection.php
@@ -2,7 +2,6 @@
 
 namespace Tabula17\Satelles\Utilis\Collection;
 
-use Tabula17\Satelles\Utilis\Collection\GenericCollection;
 use Tabula17\Satelles\Utilis\Config\ConnectionConfig;
 
 class ConnectionCollection extends GenericCollection
@@ -15,6 +14,41 @@ class ConnectionCollection extends GenericCollection
         $this->values = $connections;
     }
 
+    /**
+     * @param string $name
+     * @return ConnectionConfig|null
+     */
+    public function findByName(string $name): ConnectionConfig|null
+    {
+        return $this->findBy('name', $name);
+    }
+    public function removeByName(string $name): void
+    {
+        $this->removeBy('name', $name);
+    }
+
+    public function filterByHost(string $name): self
+    {
+        return $this->filterBy('host', $name);
+    }
+
+    public function findBy(string $key, mixed $value)
+    {
+        return $this->find(fn(ConnectionConfig $config) => $config->$key === $value);
+    }
+
+    public function filterBy(string $key, mixed $value): self
+    {
+        return $this->filter(fn(ConnectionConfig $config) => $config->$key === $value);
+    }
+    public function removeBy(string $key, mixed $value): void
+    {
+        $this->remove($this->findBy($key, $value));
+    }
+    public function collect(string $key): array
+    {
+        return array_filter(array_map(static fn(ConnectionConfig $config) => $config->$key, $this->values));
+    }
     public static function fromArray(array $config): self
     {
         return new self(...array_map(static fn($item) => $item instanceof self::$type ? $item : new self::$type($item), $config));
