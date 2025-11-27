174c617350751692049a1d50650e0d7469b208eb
Martín Panizzo <marpan@papeleratucuman.com.ar>
2025-11-27 16:03:17 -0300

    Add `TCPServerCollection` with host-based filtering and modification methods

---
+++ src/Collection/TCPServerCollection.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tabula17\Satelles\Utilis\Collection;
+
+use Tabula17\Satelles\Utilis\Config\TCPServerConfig;
+
+class TCPServerCollection extends GenericCollection
+{
+    private static string $type = TCPServerConfig::class;
+
+    public function __construct(TCPServerConfig...$connections)
+    {
+        $this->values = $connections;
+    }
+    /**
+     * @param string $host
+     * @return TCPServerConfig|null
+     */
+    public function findByHost(string $host): TCPServerConfig|null
+    {
+        return $this->findBy('host', $host);
+    }
+    public function removeByHost(string $host): void
+    {
+        $this->removeBy('host', $host);
+    }
+    public function filterByHost(string $host): self
+    {
+        return $this->filterBy('host', $host);
+    }
+
+    public function findBy(string $key, mixed $value)
+    {
+        return $this->find(fn(TCPServerConfig $config) => $config->$key === $value);
+    }
+
+    public function filterBy(string $key, mixed $value): self
+    {
+        return $this->filter(fn(TCPServerConfig $config) => $config->$key === $value);
+    }
+    public function removeBy(string $key, mixed $value): void
+    {
+        $this->remove($this->findBy($key, $value));
+    }
+    public function collect(string $key): array
+    {
+        return array_filter(array_map(static fn(TCPServerConfig $config) => $config->$key, $this->values));
+    }
+    public static function fromArray(array $config, ?string $type = null): self
+    {
+        $class = $type ?? static::$type;
+        return new self(...array_map(static fn($item) => $item instanceof $class ? $item : new $class($item), $config));
+    }
+}
