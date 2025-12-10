d7adc01bc3bd8669cf63239bf8586a146e5b25f7
Martín Panizzo <marpan@papeleratucuman.com.ar>
2025-12-10 13:01:15 -0300

    - Make `TypedCollection` abstract and enforce type definition via exception - Introduce `UnexpectedValueException` for better error handling - Refactor `TypedCollection` to improve type validation logic

--- src/Collection/TypedCollection.php
+++ src/Collection/TypedCollection.php
@@ -3,6 +3,7 @@
 namespace Tabula17\Satelles\Utilis\Collection;
 
 use Tabula17\Satelles\Utilis\Collection\GenericCollection;
+use Tabula17\Satelles\Utilis\Exception\UnexpectedValueException;
 
 /**
  * Represents a typed collection that enforces all items to be instances of a specified type.
@@ -11,19 +12,28 @@ use Tabula17\Satelles\Utilis\Collection\GenericCollection;
  * for the contained elements. The type is defined at runtime and validated upon creation of the collection,
  * ensuring that all elements conform to the defined type.
  */
-class TypedCollection extends GenericCollection
+abstract class TypedCollection extends GenericCollection
 {
     protected static string $type;
 
-    public function __construct(string $type, array $values)
+    /**
+     * @throws UnexpectedValueException
+     */
+    public function __construct(array $values)
     {
-        self::$type = $type;
+        if(!isset(static::$type)) {
+            throw new UnexpectedValueException('Type must be defined for TypedCollection');
+        }
         array_walk($values, static fn($value) => $value instanceof self::$type ?: new self::$type($value));
         $this->values = $values;
     }
+
+    /**
+     * @throws UnexpectedValueException
+     */
     public static function fromArray(array $config, ?string $type = null): self
     {
         $class = $type ?? static::$type;
-        return new self($class, ...array_map(static fn($item) => $item instanceof $class ? $item : new $class($item), $config));
+        return new static(...array_map(static fn($item) => $item instanceof $class ? $item : new $class($item), $config));
     }
 }
