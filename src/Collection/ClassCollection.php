6c029c9bb07b03cd47f95ef1b3fa57607c95eec0
Martín <martin17@gmail.com>
2026-04-03 00:31:15 -0300

    Refactor `ClassCollection` to improve type validation and enhance methods with stricter checks

--- src/Collection/ClassCollection.php
+++ src/Collection/ClassCollection.php
@@ -12,17 +12,20 @@ class ClassCollection extends GenericCollection
         array_filter($values, static fn($value) => static::checkType($value));
         $this->values = $values;
     }
+
     protected static function checkType($value): string|false
     {
-        if(is_object($value)) {
+        if (is_object($value)) {
             $value = get_class($value);
         }
         return class_exists($value);
     }
+
     public static function fromArray(array $config, ?string $type = null): static
     {
         return new static(...$config);
     }
+
     public function add(mixed $value): void
     {
         $this->values[] = static::checkType($value);
@@ -30,25 +33,29 @@ class ClassCollection extends GenericCollection
 
     public function offsetSet(mixed $offset, mixed $value): void
     {
-        $value = static::checkType($value);
-        if ($offset === null) {
-            $this->values[] = $value;
-        } else {
-            $this->values[$offset] = $value;
+        if (static::checkType($value)) {
+            if ($offset === null) {
+                $this->values[] = $value;
+            } else {
+                $this->values[$offset] = $value;
+            }
         }
     }
 
 
     public function set(mixed $key, mixed $value): void
     {
-        $this->values[$key] =  static::checkType($value);
+        if (static::checkType($value)) {
+            $this->values[$key] = static::checkType($value);
+        }
     }
 
 
     public function addIfNotExist(mixed $value): void
     {
-        if (!in_array($value, $this->values, true)) {
-            $this->values[] =  static::checkType($value);
+        if (static::checkType($value) && !in_array($value, $this->values, true)) {
+            $this->values[] = static::checkType($value);
         }
+
     }
 }
