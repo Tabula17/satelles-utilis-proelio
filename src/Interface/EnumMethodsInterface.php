0492caa4cb11a5ecf82c09b1084056bf46717fea
Martín <martin17@gmail.com>
2026-04-22 09:37:52 -0300

    Update `EnumMethodsInterface::fromValue` return type from `static` to `self` for improved clarity and consistency.

--- src/Interface/EnumMethodsInterface.php
+++ src/Interface/EnumMethodsInterface.php
@@ -4,7 +4,7 @@ namespace Tabula17\Satelles\Utilis\Interface;
 
 interface EnumMethodsInterface extends \UnitEnum
 {
-    public static function fromValue(mixed $value): ?static;
+    public static function fromValue(mixed $value): ?self;
     public static function isValid(mixed $value): bool;
 
 }
