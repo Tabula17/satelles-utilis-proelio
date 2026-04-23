4a0fe2b8c749dbe8fbf893d35b5d07528dcf2d97
Martín <martin17@gmail.com>
2026-04-23 15:20:50 -0300

    Add `fromArray` abstract method to `AbstractJob` class

--- src/Job/AbstractJob.php
+++ src/Job/AbstractJob.php
@@ -7,4 +7,5 @@ use Tabula17\Satelles\Utilis\Config\AbstractDescriptor;
 abstract class AbstractJob extends AbstractDescriptor implements JobInterface
 {
     public readonly string $jobId;
+    abstract public static function fromArray(array $data): static;
 }
