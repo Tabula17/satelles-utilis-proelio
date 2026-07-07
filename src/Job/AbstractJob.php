c0ead0e802af0609f465428cb686ee3921e0ef0c
Martín <martin17@gmail.com>
2026-07-07 10:19:43 -0300

    Restrict `jobId` mutation in `AbstractJob` and `AbstractJobResult` by introducing protected setter with initialization guard.

--- src/Job/AbstractJob.php
+++ src/Job/AbstractJob.php
@@ -6,6 +6,14 @@ use Tabula17\Satelles\Utilis\Config\AbstractDescriptor;
 
 abstract class AbstractJob extends AbstractDescriptor implements JobInterface
 {
-    public readonly string $jobId;
+    //public readonly string $jobId;
+    protected(set) string $jobId {
+        set {
+            if (isset($this->jobId)) {
+                throw new \Error("Cannot modify initialized property AbstractJob::\$jobId");
+            }
+            $this->jobId = $value;
+        }
+    }
     //abstract public static function fromArray(array $data): static;
 }
--- src/Job/AbstractJobResult.php
+++ src/Job/AbstractJobResult.php
@@ -6,6 +6,14 @@ use Tabula17\Satelles\Utilis\Config\AbstractDescriptor;
 
 abstract class AbstractJobResult extends AbstractDescriptor implements JobResultInterface
 {
-    public readonly string $jobId;
+    //public readonly string $jobId;
+    protected(set) string $jobId {
+        set {
+            if (isset($this->jobId)) {
+                throw new \Error("Cannot modify initialized property AbstractJobResult::\$jobId");
+            }
+            $this->jobId = $value;
+        }
+    }
 
 }
