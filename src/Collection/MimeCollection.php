bc686e1c0dc244d7a6b06bcd032b8eb668394437
Martín <martin17@gmail.com>
2026-07-17 11:39:43 -0300

    Add MimeCollection class for handling MIME types and extensions

---
+++ src/Collection/MimeCollection.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tabula17\Satelles\Utilis\Collection;
+
+
+use Tabula17\Satelles\Utilis\File\MimeTypes;
+use Throwable;
+
+class MimeCollection extends TypedEnumCollection
+{
+
+    protected static function getEnumType(): string
+    {
+        return MimeTypes::class;
+    }
+
+    public function getMimeTypes(): array
+    {
+        return $this->map(fn(MimeTypes $mime) => $mime->mime());
+    }
+
+    public function getExtensions(): array
+    {
+        return $this->map(fn(MimeTypes $mime) => $mime->extension());
+    }
+
+    public function getMimeTypesAndExtensions(): array
+    {
+        return array_combine($this->getMimeTypes(), $this->getExtensions());
+    }
+
+    public static function fromArray(array $config): static
+    {
+        $values = [];
+
+        foreach ($config as $key => $item) {
+            try {
+                $values[$key] = static::cast($item);
+            } catch (Throwable $e) {
+                continue;
+            }
+        }
+
+        return new static(...$values);
+    }
+}
