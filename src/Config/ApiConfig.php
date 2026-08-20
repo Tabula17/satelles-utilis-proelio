20f48acf38b8b33d55be92e62531949325d410d4
Martín <martin17@gmail.com>
2026-08-20 15:28:36 -0300

    Update header normalization in `ApiConfig` to include keys in the normalization process

--- src/Config/ApiConfig.php
+++ src/Config/ApiConfig.php
@@ -45,7 +45,7 @@ class ApiConfig extends ConnectionConfig
                     return str_contains($value, ":") ? ucfirst($value) : "$key: $value";
                 };
 
-                return array_map($normalizer, $this->headers);
+                return array_map($normalizer, $this->headers, array_keys($this->headers));
             }
         }
     protected(set) ApiPathsCollection $apiPaths
