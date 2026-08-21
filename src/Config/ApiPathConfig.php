37b7e434b3d2a43c5ec304fed9a5440c2a514cd8
Martín <martin17@gmail.com>
2026-08-20 21:51:30 -0300

    Fix `getPathParamsString` in `ApiPathConfig` to resolve incorrect `array_map` usage for placeholder processing

--- src/Config/ApiPathConfig.php
+++ src/Config/ApiPathConfig.php
@@ -78,7 +78,8 @@ class ApiPathConfig extends AbstractDescriptor
 
     public function getPathParamsString(bool $onlyValid = true): string
     {
-        return implode('/', $this->params->getPlaceholders($onlyValid));
+        $placeholders = $this->params->getPlaceholders($onlyValid);
+        return implode('/', array_map(fn($key, $value) => $value, array_keys($placeholders), array_values($placeholders)));
     }
 
     public function getEndpoint(string $baseUrl = '', bool $withParamPath = false, bool $onlyValid = true): string
