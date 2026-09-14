5d38654ccb7072585caad7c607e6c239eb0e0bec
Martín <martin17@gmail.com>
2026-09-14 13:02:22 -0300

    Make `baseUrl` mutable in `ApiPathConfig` with added setter logic

--- src/Collection/ApiPathsCollection.php
+++ src/Collection/ApiPathsCollection.php
@@ -44,8 +44,9 @@ class ApiPathsCollection extends TypedCollection
                 if ($baseUrl) {
                     $item['baseUrl'] = $baseUrl;
                 }
-                echo $key . '=>>' . var_export($item, true) . PHP_EOL;
+               // echo $key . '=>>' . var_export($item, true) . PHP_EOL;
                 $values[$key] = static::cast($item, ApiPathConfig::class, true);
+                echo $key . '<<<<' . get_class($values[$key]) . PHP_EOL;
             } catch (Throwable $e) {
                 continue;
             }
